ba9797667d9241f5ec4bb38052f7801050cf67f0
Maarten van Gompel <proycon@anaproy.nl>
2020-02-06 16:07:57 +0100

    added public key

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 #use custom hugo
 export GOPATH=~/go
 cp auth* public/
+cp key.asc public/
 $GOPATH/bin/hugo && rsync -avz --delete public/ worker.anaproy.lxd:/home/www/proycon.anaproy.nl/html
 exit $?
