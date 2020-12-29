03b745f398a4f5e2ed3e2c1db229048504cb1868
Maarten van Gompel <proycon@anaproy.nl>
2020-12-29 15:58:08 +0100

    added option to skip build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-zola build || exit 1
+if [ "$1" != "-n" ]; then
+    zola build || exit 1
+fi
 cp auth* public/
 cp key.asc public/
 rsync -avz --delete public/ worker.anaproy.lxd:/home/www/proycon.anaproy.nl/html
