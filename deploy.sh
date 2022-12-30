693b1e746f0182a48c21aff32f4a3a7ec9cf84b3
Maarten van Gompel <proycon@anaproy.nl>
2022-12-30 22:48:46 +0100

    added ssh pubkey and vcard

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ if [ "$1" != "-n" ]; then
 fi
 cp auth* public/
 cp key.asc public/
+cp proycon.vcf public/
+cp id_ed25519.pub public/
 rsync -avz --delete public/ worker.anaproy.lxd:/home/www/proycon.anaproy.nl/html
 exit $?
